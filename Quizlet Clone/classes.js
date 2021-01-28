909004819ade0000f94b85936545d7274afee0e1
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-28 18:42:14 -0500

    Add files via upload

---
+++ Quizlet Clone/classes.js
@@ -0,0 +1,19 @@
+export class Main {
+
+    constructor(Obj1, Obj2, Obj3) {
+        this.ObjectOne = Obj1;
+        this.ObjectTwo = Obj2;
+        this.ObjectThree = Obj3;
+    }
+    methodOne(){
+        //
+    };
+    methodTwo(){
+        //
+    };
+};
+
+
+class SubMain extends Main {
+    //
+};
---
+++ Quizlet Clone/main.js
@@ -0,0 +1,5 @@
+import { Main } from "./classes.js";
+
+window.addEventListener("DOMContentLoaded", ()=>{setTimeout(()=>{document.getElementById('main_header').style.display = 'block';}, 2000)});
+
+console.log(Main);
