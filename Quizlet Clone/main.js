6bbe7ba173b668edcc8b9519c6cee0b0ad900420
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-29 00:57:17 -0500

    Update main.js

--- Quizlet Clone/main.js
+++ Quizlet Clone/main.js
@@ -1,5 +1,20 @@
-import { Main } from "./classes.js";
+//                                      ||   IMPORTS   ||                                      \\
+import { Component } from "./classes.js";
 
-window.addEventListener("DOMContentLoaded", ()=>{setTimeout(()=>{document.getElementById('main_header').style.display = 'block';}, 2000)});
 
-console.log(Main);
+//                                      ||   CONSTANTS FOR WEBPAGE DIVISIONS   ||                                      \\
+const HEADER = document.getElementById('header_container');
+const BODY = document.getElementById('page_container');
+    const HOME = document.getElementById('body_background');
+        const H_POSTS = document.getElementById('load_message');
+    const CATEGORIES = document.getElementById('category_side');
+    const SUB_FORM = document.getElementById('update_form');
+
+
+//                                      ||   CLASSES   ||                                      \\
+const HEADER_CPT = new Component({Main: {text: "", isActive: true}, Alt: ""})
+console.log(HEADER_CPT.cptName);
+
+
+//                                      ||      ||                                      \\
+// CODE HERE
