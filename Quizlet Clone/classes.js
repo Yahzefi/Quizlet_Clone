45aa6eeac100eeb9faf65692dbb23d0ecbb0bd49
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-29 16:29:30 -0500

    Update classes.js

--- Quizlet Clone/classes.js
+++ Quizlet Clone/classes.js
@@ -1,15 +1,42 @@
 export class Component {
-    constructor(componentName, componentTierLevel, componentHasAnimations, componentAutoFill, componentStorage) {
+    constructor(componentName, componentTierLevel, componentHasAnimations, componentHasStorage) {
         this.cptName = componentName;
         this.cptTierLVL = componentTierLevel;
         this.cptAnim = componentHasAnimations;
-        this.cptFill = componentAutoFill;
-        this.cptStg = componentStorage;
+        this.cptStg = componentHasStorage;
+    };
+    attachSubCPT(child){
+        return new Promise(resolve=>{
+            resolve(this.cptName.appendChild(child));
+        });
+    };
+    async attachTwoSubCPT(firstChild, secondChild){
+        await this.attachSubCPT(firstChild)
+        .then((data)=>{
+            let selectedCPT = data;
+            selectedCPT.appendChild(secondChild)
+        });
     }
-    methodOne(){
-        //
-    };
-    methodTwo(){
-        //
-    };
 };
+
+export class Subcomponent {
+    constructor(type, id, className, hasInLineStyling) {
+        this.type = type;
+        this.id = id;
+        this.className = className;
+        this.hasInLineStyling = hasInLineStyling;
+    }
+    createElement(){
+        let newElement = document.createElement(this.type);
+        newElement.setAttribute('id', this.id);
+        newElement.setAttribute('class', this.className);
+        return newElement;
+    }
+    insertStyling() {
+        if(this.hasInLineStyling){
+            //
+        } else {
+            //
+        }
+    }
+}
