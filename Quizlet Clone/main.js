ab86c613b3b684e99ec55988db792360d5906bf6
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-29 16:28:53 -0500

    Update main.js

--- Quizlet Clone/main.js
+++ Quizlet Clone/main.js
@@ -1,6 +1,5 @@
 //                                      ||   IMPORTS   ||                                      \\
-import { Component } from "./classes.js";
-
+import { Component, Subcomponent } from "./classes.js";
 
 //                                      ||   CONSTANTS FOR WEBPAGE DIVISIONS   ||                                      \\
 const HEADER = document.getElementById('header_container');
@@ -8,13 +7,26 @@ const BODY = document.getElementById('page_container');
     const HOME = document.getElementById('body_background');
         const H_POSTS = document.getElementById('load_message');
     const CATEGORIES = document.getElementById('category_side');
-    const SUB_FORM = document.getElementById('update_form');
+const FORM = document.getElementById('form_container');
+    const SUB_FORM = document.getElementById('subscription_form');
 
+//                                      ||   INSTANTIATIONS   ||                                      \\
 
-//                                      ||   CLASSES   ||                                      \\
-const HEADER_CPT = new Component({Main: {text: "", isActive: true}, Alt: ""})
-console.log(HEADER_CPT.cptName);
+// Components (CONTAINERS)
+let headerCPT = new Component(HEADER, 1, false, false);
+let bodyCPT = new Component(BODY, 2, false, false);
+let formCPT = new Component(FORM, 3, false, true);
 
+// Subcomponents (CHILDREN ELEMENTS)
+let home_SubCPT = new Subcomponent("div", "home_container", "home-container", false);
+    let posts_SubCPT = new Subcomponent("div", "post_container", "post-container", false);
+let ctg_SubCPT = new Subcomponent("div", "category_container", "ctg-container", false);
+let form_subCPT = new Subcomponent("form", "subscription_form", "sub-form", false);
+    let subForm_subCPT = new Subcomponent("input", "", "", false);
 
-//                                      ||      ||                                      \\
-// CODE HERE
+//                                      ||   DOM MANIPULATIONS   ||                                      \\
+formCPT.attachTwoSubCPT(
+    form_subCPT.createElement(), 
+    subForm_subCPT.createElement()
+);
+
