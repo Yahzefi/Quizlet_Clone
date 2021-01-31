28645a5dc787ffc32ba0646965f1db84264fa935
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-30 21:05:16 -0500

    Update main.js

--- Quizlet Clone/main.js
+++ Quizlet Clone/main.js
@@ -1,10 +1,13 @@
 //                                      ||   IMPORTS   ||                                      \\
 
 import { Component, Subcomponent } from "./Classes/Components.js";
+import { headerMain } from "./Header/header.js";
 
 //                                      ||   CONSTANTS FOR WEBPAGE DIVISIONS   ||                                      \\
 
 const HEADER = document.getElementById('header_container');
+    const LOGIN_CREDS = document.getElementById('logIn_Links');
+    const PAGE_NAV = document.getElementById('page_navigation');
 const BODY = document.getElementById('page_container');
 const FORM = document.getElementById('form_container');
 
@@ -18,6 +21,8 @@ let formCPT = new Component(FORM, 3, false, true);
 
 // Subcomponents (CHILDREN ELEMENTS)
 
+let logInLinks_SubCPT = new Subcomponent(LOGIN_CREDS.tagName, LOGIN_CREDS.id, LOGIN_CREDS.className, false);
+let pageNav_SubCPT = new Subcomponent(PAGE_NAV.tagName, PAGE_NAV.id, PAGE_NAV.className, false);
 let home_SubCPT = new Subcomponent("div", "home_container", "home-container", false);
     let posts_SubCPT = new Subcomponent("div", "post_container", "post-container", false);
 let ctg_SubCPT = new Subcomponent("div", "category_container", "ctg-container", false);
@@ -46,7 +51,7 @@ function initWebLayout(){
             ctg_SubCPT.createElement()
         );
         formCPT.attachTwoSubCPT(
-            form_subCPT.createElement(), 
+            form_subCPT.createElement(),
             subForm_subCPT.createElement()
         );
         resolve();
@@ -56,6 +61,8 @@ function initWebLayout(){
 // Returns an Object Containing ID For All Subcomponent Elements \\
 function createSubObj(){
     let passedData = {
+        logIn: logInLinks_SubCPT.id,
+        pageNav: pageNav_SubCPT.id,
         home: home_SubCPT.id,
             posts: posts_SubCPT.id,
         categories: ctg_SubCPT.id,
@@ -69,20 +76,28 @@ function createSubObj(){
 function initPageFill(data){
 // All Necessary HTML Element Reference Variables
     let headerDiv, bodyDiv, formDiv;
-    let homeDiv, postsDiv, categoryDiv;
+    let logInDiv, pageNavDiv, homeDiv, postsDiv, categoryDiv;
     let subForm, nameInput;
 
     headerDiv = headerCPT.cptName;
+        logInDiv = document.getElementById(data.logIn);
+        pageNavDiv = document.getElementById(data.pageNav);
+
+    let headerCollection = [headerDiv, logInDiv, pageNavDiv];
 
     bodyDiv = bodyCPT.cptName;
         homeDiv = document.getElementById(data.home);
             postsDiv = document.getElementById(data.posts);
         categoryDiv = document.getElementById(data.categories);
 
+    let bodyCollection = [bodyDiv, homeDiv, postsDiv, categoryDiv];
+
     formDiv = formCPT.cptName;
         subForm = document.getElementById(data.form);
             nameInput = document.getElementById(data.nameInput);
 
-// START CODE HERE
+    let formCollection = [formDiv, subForm, nameInput];
 
+// START CODE HERE
+    headerMain(headerCollection);
 };
