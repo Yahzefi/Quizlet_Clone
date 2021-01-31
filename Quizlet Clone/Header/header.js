871ce8a6e783010a71c251f545ff0f77530cf2c7
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-01-30 21:06:22 -0500

    Update header.js

--- Quizlet Clone/Header/header.js
+++ Quizlet Clone/Header/header.js
@@ -1,1 +1,11 @@
-// 
+//                                      ||   IMPORTS   ||                                      \\
+
+import { Component, Subcomponent } from "../Classes/Components.js";
+
+//                                      ||   LINKED FUNCTION   ||                                      \\
+
+export function headerMain(data){
+    data.forEach((item)=>{
+        console.log(item);
+    })
+};
