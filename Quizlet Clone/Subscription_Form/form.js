d7f35ef7cc1da839a7c93fa3f9ad386156767461
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-03 13:42:56 -0500

    Update form.js

--- Quizlet Clone/Subscription_Form/form.js
+++ Quizlet Clone/Subscription_Form/form.js
@@ -3,28 +3,43 @@ import { Component, Subcomponent } from "../Classes/Components.js";
 export function formMain(data) {
 // TRANSFER PARAMETER DATA ARRAY
     let formArr = data;
+    console.log("form");
+
 // SPLIT PARAMATER ARRAY
     let formDiv, formElement, nameInput;
-    formDiv = accessArray(0, formArr);
-    formElement = accessArray(1, formArr);
-    nameInput = accessArray(2, formArr);
+    formDiv = accessArray("div", formArr);
+    formElement = accessArray("form", formArr);
+    nameInput = accessArray("nameInput", formArr);
+
+
 // COMPONENTS
     let formCPT = new Component(formDiv, '', false, false);
+
 // SUBCOMPONENTS
-    let nameLabel = new Subcomponent('label', 'name_label', '', false);
+    let nameLabel = new Subcomponent('label', 'name_label', 'form-label', false);
+    let emailLabel = new Subcomponent('label', 'email_label', 'form-label', false);
+
+    let emailInput = new Subcomponent('input', 'email_input', 'txt-inp', false);
 
 
 // CODE GOES HERE
-    formElement.insertBefore(nameLabel.createElement("for", ["nameInput", "Name:"]), nameInput);
+    // JS CODE FOR INSERTED DOM ELEMENT BEFORE AN ALREADY PRESENT ELEMENT
+    // formElement.insertBefore(nameLabel.createElement());
+    
+    // JQUERY CODE FOR THE ABOVE FUNCTIONALITY
+    $(nameInput).before(nameLabel.createElement(true, [{reqAttr:"for", reqInfo:"nameInput", innerTxt:"Name:"}]));
+    $("<br>").appendTo(formElement);
+    $(formElement).append(emailLabel.createElement(true, [{reqAttr:"for", reqInfo:"email-label", innerTxt:"Email:"}]));
+    $(formElement).append(emailInput.createElement(true, [{reqAttr:"type", reqInfo:"email"}, {reqAttr:"name", reqInfo:"email-label"}]));
 }
 
 function accessArray(tgt, formArr){
     switch(tgt){
-        case 0:
+        case "div":
             return formArr[0];
-        case 1:
+        case "form":
             return formArr[1];
-        case 2:
+        case "nameInput":
             return formArr[2];
         default:
             console.log("Error");
