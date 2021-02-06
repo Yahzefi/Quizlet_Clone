9242955bd803902c1e78e25670ff071d9cf5ce4e
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-06 02:02:25 -0500

    Update Component.js

--- Quizlet Clone/Initialization/Component.js
+++ Quizlet Clone/Initialization/Component.js
@@ -7,17 +7,19 @@ export class Component {
         this.cptStg = hasStorage;
     };
     createContainer(){
-        if(this.cptTagName === "div" || this.cptTagName === "section"){
-            let newElement = document.createElement(this.cptTagName);
-            newElement.setAttribute('id', this.cpt_ID);
-            newElement.setAttribute('class', this.cpt_Class);
-            if(this.hasInLineStyling){
-                this.insertStyling();
-            };
-            return newElement;
-        } else {
-            throw new Error('Component Must Be A Container Element ("div" or "section")');
-        };
+        return new Promise((resolve, reject)=>{
+            if(this.cptTagName === "div" || this.cptTagName === "section"){
+                let newElement = document.createElement(this.cptTagName);
+                newElement.setAttribute('id', this.cpt_ID);
+                newElement.setAttribute('class', this.cpt_Class);
+                if(this.hasInLineStyling){
+                    this.insertStyling();
+                };
+                resolve(newElement);
+            } else {
+                reject('Component Must Be A Container Element ("div" or "section")');
+            }
+        })
     };
     createAnimations() {
         if(this.cpt_hasStyling){
@@ -26,27 +28,6 @@ export class Component {
             console.log("The " + this.cptTagName.id + " does NOT have animations toggled.")
         };
     };
-    attachSubCPT(child){
-        return new Promise(resolve=>{
-            resolve(this.cptTagName.appendChild(child));
-        });
-    };
-    async attachTwoSubCPT(firstChild, secondChild){
-        return new Promise(res=>{
-            this.attachSubCPT(firstChild)
-            .then((data)=>{
-                let selectedCPT = data;
-                res(selectedCPT.appendChild(secondChild));
-            });
-        })
-    }
-    async attachThreeSubCPT(f_Child, s_Child, t_Child){
-        this.attachTwoSubCPT(f_Child, s_Child)
-        .then((data)=>{
-            let selectedCPT = data;
-            selectedCPT.appendChild(t_Child);
-        });
-    }
     insertStyling() {
         if(this.hasInLineStyling){
             
