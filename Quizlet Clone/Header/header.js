e895c1fe20a194a1f1729af1586ea35bb30f1db0
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-06 02:01:04 -0500

    Update header.js

--- Quizlet Clone/Header/header.js
+++ Quizlet Clone/Header/header.js
@@ -1,11 +1,44 @@
 //                                      ||   IMPORTS   ||                                      \\
 
-import { Component, Subcomponent } from "../Classes/Components.js";
+import { Component } from "../Initialization/Component.js";
+import { Subcomponent } from "../Initialization/Subcomponent.js";
 
 //                                      ||   LINKED FUNCTION   ||                                      \\
 
 export function headerMain(data){
-    data.forEach((item)=>{
-        console.log(item);
-    })
+    let header_Collection = data;
+    console.log(header_Collection);
+
+    $('#log_A').click((e)=>navLink(e));
+    $('#reg_A').click((e)=>navLink(e));
+
+    $('#navOne').click((e)=>navLink(e))
+    $('#navTwo').click((e)=>navLink(e))
+    $('#navThree').click((e)=>navLink(e))
 };
+
+// NAVIGATION REDIRECT
+
+function navLink(e){
+    switch(e.target.id){
+        case "log_A":
+            console.log("log in");
+            break;
+        case "reg_A":
+            console.log("register");
+            break;
+        case "navOne":
+            console.log("Nav One");
+            break;
+        case "navTwo":
+            console.log("Nav Two");
+            break;
+        case "navThree":
+            console.log("Nav Three");
+            break;
+        case "":
+            break;
+        default:
+            throw new Error("Route Not Found");
+    }
+}
