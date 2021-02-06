c87f2b49198e67309561faaf995b7f530fff479b
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-06 02:01:55 -0500

    Update form.js

--- Quizlet Clone/Subscription_Form/form.js
+++ Quizlet Clone/Subscription_Form/form.js
@@ -3,46 +3,33 @@ import { Subcomponent } from "../Initialization/Subcomponent.js";
 
 export function formMain(data) {
 // TRANSFER PARAMETER DATA ARRAY
-    let formArr = data;
-    console.log("form");
+    let form_Collection = data;
+    console.log(form_Collection);
 
 // SPLIT PARAMATER ARRAY
-    let formDiv, formElement, nameInput;
-    formDiv = accessArray("div", formArr);
-    formElement = accessArray("form", formArr);
-    nameInput = accessArray("nameInput", formArr);
-
-
-// COMPONENTS
-//
-
-// SUBCOMPONENTS
-    let nameLabel = new Subcomponent('label', 'name_label', 'form-label', false);
-    let emailLabel = new Subcomponent('label', 'email_label', 'form-label', false);
-
-    let emailInput = new Subcomponent('input', 'email_input', 'txt-inp', false);
-
-
-// CODE GOES HERE
-    // JS CODE FOR INSERTED DOM ELEMENT BEFORE AN ALREADY PRESENT ELEMENT
-    // formElement.insertBefore(nameLabel.createElement());
-    
-    // JQUERY CODE FOR THE ABOVE FUNCTIONALITY
-    $(nameInput).before(nameLabel.createElement([{reqAttr:"for", reqInfo:"nameInput", innerTxt:"Name:"}]));
-    $("<br>").appendTo(formElement);
-
-    $(formElement).append(emailLabel.createElement([{reqAttr:"for", reqInfo:"email-label", innerTxt:"Email:"}]));
-    $(formElement).append(emailInput.createElement([{reqAttr:"type", reqInfo:"email"}, {reqAttr:"name", reqInfo:"email-label"}]));
+    let formDiv, formElement, nameLabel, nameInput, emailLabel, emailInput;
+    formDiv = accessArray("div", form_Collection);
+        formElement = accessArray("form", form_Collection);
+            nameLabel = accessArray("nameLabel", form_Collection);
+            nameInput = accessArray("nameInput", form_Collection);
+            emailLabel = accessArray("emailLabel", form_Collection);
+            emailInput = accessArray("emailInput", form_Collection);
 }
 
-function accessArray(tgt, formArr){
+function accessArray(tgt, form_Collection){
     switch(tgt){
         case "div":
-            return formArr[0];
+            return form_Collection["container"];
         case "form":
-            return formArr[1];
+            return form_Collection["children"].form;
+        case "nameLabel":
+            return form_Collection["children"].formChildren["nameLabel"];
         case "nameInput":
-            return formArr[2];
+            return form_Collection["children"].formChildren["nameInput"];
+        case "emailLabel":
+            return form_Collection["children"].formChildren["emailLabel"];
+        case "emailInput":
+            return form_Collection["children"].formChildren["emailInput"];
         default:
             console.log("Error");
     }
