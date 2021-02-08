55511713a58224c4bd65a4d139e2693092196f41
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-08 01:16:12 -0500

    Update categories.js

--- Quizlet Clone/Categories/categories.js
+++ Quizlet Clone/Categories/categories.js
@@ -1,1 +1,13 @@
-// 
+//                                      ||   IMPORTS   ||                                      \\
+
+
+
+//                                      ||   LINKED FUNCTION   ||                                      \\
+
+export function categoryMain(data){
+    // PASS PARAM INTO SCOPED VARIABLE
+    const CATEGORY_COLLECTION = data;
+
+    // PULL EACH ELEMENT OF COLLECTION AREA
+    
+}
