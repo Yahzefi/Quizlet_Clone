2b6ec1b0caf9f1f7d4c1c9db6c7a306f5719463e
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-11 22:05:55 -0500

    Add files via upload

---
+++ Quizlet Clone/About/main.about.js
@@ -0,0 +1,5 @@
+// ANIMATIONS
+
+// Changing position once DOM is loaded causes glitch-type response when page is loaded.  Gonna have to change it in stylesheet and fix it to correct positioning later in js file
+
+$(document).ready(()=>{});
