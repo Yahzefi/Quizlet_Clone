9c44e93b16965ddae3138867bb11126f9f2b4358
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-14 20:03:20 -0500

    Update on_Init.js

--- Quizlet Clone/Initialization/on_Init.js
+++ Quizlet Clone/Initialization/on_Init.js
@@ -14,6 +14,15 @@ let formCPT = new Component("div", "form_container", "form-container");
 // Subcomponents (CHILDREN ELEMENTS)
 
 // HEADER
+    let logInModal_SC = new Subcomponent("div", "log_modal", "log-modal_container");
+    let userNavBar_SC = new Subcomponent("nav", "user_nav", "user-nav");
+        let userProfile_SC = new Subcomponent("span", "user_profile", "user-profile");
+        let profileDropDown_SC = new Subcomponent("div", "user_dropdown", "user-drop");
+            let profileOptionOne_SC = new Subcomponent("span", "opt_1", "user-menuSelect");
+            let profileOptionTwo_SC = new Subcomponent("span", "opt_2", "user-menuSelect");
+            let profileOptionThree_SC = new Subcomponent("span", "opt_3", "user-menuSelect");
+        let userLibrary_SC = new Subcomponent("span", "user_library", "user-span");
+        let userCreateSet_SC = new Subcomponent("span", "user_newSet", "user-span");
     let logInLinks_SC = new Subcomponent('div', 'logIn_Links', 'log-links');
         let mainHeadTxt_SC = new Subcomponent('h1', 'main_header', 'm_Header');
         let logNav_SC = new Subcomponent('nav', 'logIn_Nav', '');
@@ -26,6 +35,7 @@ let formCPT = new Component("div", "form_container", "form-container");
             let tempNavOne_SC = new Subcomponent('a', 'navOne', '');
             let tempNavTwo_SC = new Subcomponent('a', 'navTwo', '');
             let tempNavThree_SC = new Subcomponent('a', 'navThree', '');
+            let tempNavFour_SC = new Subcomponent('a', 'navFour', '');
 // BODY
     let modalBox1_SC = new Subcomponent("div", "modalBox_1", "modal-box");
     let modalBox2_SC = new Subcomponent("div", "modalBox_2", "modal-box");
@@ -39,6 +49,7 @@ let formCPT = new Component("div", "form_container", "form-container");
         let nameInput_SC = new Subcomponent("input", "name_input", "txt-inp");
         let emailLabel_SC = new Subcomponent('label', 'email_label', 'form-label');
         let emailInput_SC = new Subcomponent('input', 'email_input', 'txt-inp');
+        let submitForm_SC = new Subcomponent("input", "sub_submit", "sub-submit");
 
 
 //                                      ||   ON INIT (DOM CONSTRUCTION)   ||                                      \\
@@ -58,6 +69,8 @@ export function initWebLoad(){
                     $("<br>").appendTo('#' + form_SC.id);
                     $('#' + form_SC.id).append(emailLabel_SC.createElement([{attr: "for", content:"emailInput", innerText:"Email:"}]));
                     $('#' + form_SC.id).append(emailInput_SC.createElement([{attr:"type", content:"email"}, {attr:"name", content:"emailInput"}]));
+                    $("<br>").appendTo('#' + form_SC.id);
+                    $('#' + form_SC.id).append(submitForm_SC.createElement([{attr:"type", content:"submit"},{attr:"value", content:"Subscribe"}]));
         })
 // BODY
         bodyCPT.createContainer()
@@ -77,24 +90,39 @@ export function initWebLoad(){
         headerCPT.createContainer()
         .then((head)=>{
             $(document.body).prepend(head);
+                $(head).append(logInModal_SC.createElement());
+                $(head).append(userNavBar_SC.createElement());
+                    $('#' + userNavBar_SC.id).append(userProfile_SC.createElement([{attr:"", content:"", innerText: "Profile"}]));
+                        $('#' + userProfile_SC.id).append(profileDropDown_SC.createElement());
+                            $('#' + profileDropDown_SC.id).append(profileOptionOne_SC.createElement([{attr:"", content:"", innerText:"Option 1"}]));
+
+                            $('#' + profileDropDown_SC.id).append(profileOptionTwo_SC.createElement([{attr:"", content:"", innerText:"Option 2"}]));
+
+                            $('#' + profileDropDown_SC.id).append(profileOptionThree_SC.createElement([{attr:"", content:"", innerText:"Option 3"}]));
+                                $('#' + profileDropDown_SC.id).css("display", "none");
+                    $('#' + userNavBar_SC.id).append(userLibrary_SC.createElement([{attr:"", content:"", innerText:"My Library"}]));
+                    $('#' + userNavBar_SC.id).append(userCreateSet_SC.createElement([{attr:"", content:"", innerText:"Create New Set"}]));
                 $(head).append(logInLinks_SC.createElement());
                     $('#' + logInLinks_SC.id).append(mainHeadTxt_SC.createElement([{attr:"", content:"", innerText:"Welcome to Q-Clone"}]));
                     $('#' + logInLinks_SC.id).append(logNav_SC.createElement());
-                        $('#' + logNav_SC.id).append(logAnchor_SC.createElement([{attr:"href", content:"#", innerText: "LOGIN"}, {attr:"target", content:"_blank"}]));
+                        $('#' + logNav_SC.id).append(logAnchor_SC.createElement([{attr:"", content:"", innerText: "LOGIN"}]));
                     $('#' + logInLinks_SC.id).append(regNav_SC.createElement());
-                        $('#' + regNav_SC.id).append(regAnchor_SC.createElement([{attr:"href", content:"#", innerText:"REGISTER"}, {attr:"target", content:"_blank"}]));
+                        $('#' + regNav_SC.id).append(regAnchor_SC.createElement([{attr:"", content:"", innerText:"REGISTER"}]));
                 $(head).append(banner_SC.createElement());
                 $(head).append(pageNavDiv_SC.createElement());
                     $('#' + pageNavDiv_SC.id).append(pageNav_SC.createElement());
-                        $('#' + pageNav_SC.id).append(tempNavOne_SC.createElement([{attr:"href", content:"#", innerText:"Nav One"}, {attr:"target", content:"_blank"}]));
+                        $('#' + pageNav_SC.id).append(tempNavOne_SC.createElement([{attr:"", content:"", innerText:"Nav One"}]));
                             $("<span>| </span>").prependTo('#' + tempNavOne_SC.id);
                             $("<span> | </span>").appendTo('#' + tempNavOne_SC.id);
-                        $('#' + pageNav_SC.id).append(tempNavTwo_SC.createElement([{attr:"href", content:"#", innerText:"Nav Two"}, {attr:"target", content:"_blank"}]));
+                        $('#' + pageNav_SC.id).append(tempNavTwo_SC.createElement([{attr:"", content:"", innerText:"Nav Two"}]));
                             $("<span>| </span>").prependTo('#' + tempNavTwo_SC.id);
                             $("<span> | </span>").appendTo('#' + tempNavTwo_SC.id);
-                        $('#' + pageNav_SC.id).append(tempNavThree_SC.createElement([{attr:"href", content:"#", innerText:"Nav Three"}, {attr:"target", content:"_blank"}]));
+                        $('#' + pageNav_SC.id).append(tempNavThree_SC.createElement([{attr:"", content:"", innerText:"Nav Three"}]));
                             $("<span>| </span>").prependTo('#' + tempNavThree_SC.id);
                             $("<span> |</span>").appendTo('#' + tempNavThree_SC.id);
+                        $('#' + pageNav_SC.id).append(tempNavFour_SC.createElement([{attr:"", content:"", innerText:"Nav Three"}]));
+                            $("<span>| </span>").prependTo('#' + tempNavFour_SC.id);
+                            $("<span> |</span>").appendTo('#' + tempNavFour_SC.id);
         });
         resolve();
     });
