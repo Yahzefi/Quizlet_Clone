894877e2f3844744a7b47f45d34368f772e770a4
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-14 20:02:52 -0500

    Update app.elements.js

--- Quizlet Clone/Initialization/app.elements.js
+++ Quizlet Clone/Initialization/app.elements.js
@@ -1,17 +1,27 @@
 export function generateElementList(){
     const HEADER = document.getElementById('header_container');
-    const LOGIN_CREDS = document.getElementById('logIn_Links');
-        const MAIN_HEADING = document.getElementById('main_header');
-        const LOGIN_NAV = document.getElementById('logIn_Nav');
-            const LOG_ANCHOR = document.getElementById('log_A');
-        const REGISTER_NAV = document.getElementById('register_Nav');
-            const REG_ANCHOR = document.getElementById('reg_A');
-    const BANNER = document.getElementById('banner_graphic');
-    const PAGENAV_DIV = document.getElementById('page_navigation');
-        const PAGE_NAV = document.getElementById('routing_nav');
-            const PAGENAV_A1 = document.getElementById('navOne');
-            const PAGENAV_A2 = document.getElementById('navTwo');
-            const PAGENAV_A3 = document.getElementById('navThree');
+        const LOGIN_MODAL = document.getElementById("log_modal");
+        const USER_NAVBAR = document.getElementById("user_nav");
+            const USER_PROFILE = document.getElementById("user_profile");
+                const USER_DROPDOWN = document.getElementById("user_dropdown");
+                    const PROFILE_OPTION_ONE = document.getElementById("opt_1");
+                    const PROFILE_OPTION_TWO = document.getElementById("opt_2");
+                    const PROFILE_OPTION_THREE = document.getElementById("opt_3");
+            const USER_LIBRARY = document.getElementById("user_library");
+            const USER_NEW_SET = document.getElementById("user_newSet");
+        const LOGIN_CREDS = document.getElementById('logIn_Links');
+            const MAIN_HEADING = document.getElementById('main_header');
+            const LOGIN_NAV = document.getElementById('logIn_Nav');
+                const LOG_ANCHOR = document.getElementById('log_A');
+            const REGISTER_NAV = document.getElementById('register_Nav');
+                const REG_ANCHOR = document.getElementById('reg_A');
+        const BANNER = document.getElementById('banner_graphic');
+        const PAGENAV_DIV = document.getElementById('page_navigation');
+            const PAGE_NAV = document.getElementById('routing_nav');
+                const PAGENAV_A1 = document.getElementById('navOne');
+                const PAGENAV_A2 = document.getElementById('navTwo');
+                const PAGENAV_A3 = document.getElementById('navThree');
+                const PAGENAV_A4 = document.getElementById('navFour');
 
 const BODY = document.getElementById('page_container');
     const MODAL_ONE = document.getElementById('modalBox_1');
@@ -27,6 +37,7 @@ const FORM_CONTAINER = document.getElementById('form_container');
         const NAME_INP = document.getElementById('name_input');
         const EMAIL_LABEL = document.getElementById('email_label');
         const EMAIL_INPUT = document.getElementById('email_input');
+        const FORM_SUBMIT = document.getElementById('sub_submit');
 
 
 
@@ -37,6 +48,24 @@ const FORM_CONTAINER = document.getElementById('form_container');
                 container: HEADER,
                 children: 
                 {
+                    logModal: LOGIN_MODAL,
+                    userNavBar: USER_NAVBAR,
+                    userNavBar_Children: 
+                    {
+                        userProfile: USER_PROFILE,
+                        userProfile_Children:
+                        {
+                            userDropDown: USER_DROPDOWN,
+                            userDropDown_Children: 
+                            {
+                                profileOptionOne: PROFILE_OPTION_ONE,
+                                profileOptionTwo: PROFILE_OPTION_TWO,
+                                profileOptionThree: PROFILE_OPTION_THREE
+                            }
+                        },
+                        userLibrary: USER_LIBRARY,
+                        userCreateSet: USER_NEW_SET
+                    },
                     credentialsDiv: LOGIN_CREDS, 
                     credDiv_Children:
                     {
@@ -61,7 +90,8 @@ const FORM_CONTAINER = document.getElementById('form_container');
                         {
                             pageNav_A1: PAGENAV_A1,
                             pageNav_A2: PAGENAV_A2,
-                            pageNav_A3: PAGENAV_A3
+                            pageNav_A3: PAGENAV_A3,
+                            pageNav_A4: PAGENAV_A4
                         }
                     }
                 }
@@ -97,7 +127,8 @@ const FORM_CONTAINER = document.getElementById('form_container');
                         nameLabel: NAME_LABEL,
                         nameInput: NAME_INP,
                         emailLabel: EMAIL_LABEL,
-                        emailInput: EMAIL_INPUT
+                        emailInput: EMAIL_INPUT,
+                        formSubmit: FORM_SUBMIT
                     }
                 }
             }
