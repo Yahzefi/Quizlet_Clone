186eb8d703e0a132220256fe69faf11b6ab3e658
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-14 19:58:34 -0500

    Update c_Redirect.js

--- Quizlet Clone/Categories/c_Redirect.js
+++ Quizlet Clone/Categories/c_Redirect.js
@@ -1,3 +1,11 @@
 export function openMathPage(){
     window.open("/Categories/c_Math/index.Math.html");
 }
+
+export function openCodingPage(){
+    window.open("/Categories/c_Coding/index.Coding.html");
+}
+
+export function openHistoryPage(){
+    window.open("/Categories/c_History/index.History.html");
+}
