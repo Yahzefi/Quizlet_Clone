82267ed891dfea19260dc0d1c185edb3d8733535
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-22 01:01:41 -0500

    Update main.about.js

--- Quizlet Clone/About/main.about.js
+++ Quizlet Clone/About/main.about.js
@@ -3,7 +3,10 @@
 $(document).ready(()=>{
     $('#about_container h1').slideDown(750);
     $('.abt-head').animate({left: "0px"}, 1500);
-    $('#about_message p').animate({left: "0px"}, 2500);
+    $('#0').animate({left: "0px"}, 2500);
+    $('#1').animate({left: "0px"}, 3000);
+    $('#2').animate({left: "0px"}, 3500);
+    $('#3').animate({left: "0px"}, 4000);
     $('#about_image').animate({opacity: "1"}, 3000);
     $('#about_image img').animate({opacity: "1"}, 3000);
 })
