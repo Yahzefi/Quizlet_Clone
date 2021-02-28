6b5d7ee6264d20f6e17b9c977dd281282c09375f
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-28 01:53:39 -0500

    Update d.Programming.js

--- Quizlet Clone/Categories/Dialogues/d.Programming.js
+++ Quizlet Clone/Categories/Dialogues/d.Programming.js
@@ -1,2 +1,15 @@
-// import { toggleDialogueBox } from "../../Initialization/Assistants.js";
+import { create_dBox } from "../../Initialization/Assistants.js";
+import { pause } from "../c_Programming/main.Programming.js";
 // DIALOGUE PATHS WITH PROGRAMMING ASSISTANT
+
+export function firstReply(){
+    create_dBox()
+    .then(async(dBox)=>{
+        await pause(500);
+        $('#white_chatDiv').append(dBox);
+        $('#dialogue_box').css("display", "none");
+        $('#dialogue_box').slideDown(1000);
+        await pause(1000);
+    })
+}
+
