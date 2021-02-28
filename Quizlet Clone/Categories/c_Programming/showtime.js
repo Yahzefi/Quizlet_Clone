eaa36e189b7bd296ecb8253a8ede828b6abee243
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-02-28 01:53:03 -0500

    Update showtime.js

--- Quizlet Clone/Categories/c_Programming/showtime.js
+++ Quizlet Clone/Categories/c_Programming/showtime.js
@@ -1,23 +1,29 @@
 //                                      ||   IMPORTS   ||                                      \\
 import { pause } from "./main.Programming.js";
 import { Subcomponent } from "../../Initialization/Subcomponent.js";
-import { Assistant } from "../../Initialization/Assistants.js";
+import { ASSISTANT_LIST } from "../../Initialization/on_Init.js";
+import { firstReply } from "../Dialogues/d.Programming.js";
 
 //                                      ||   GLOBAL VARIABLES   ||                                      \\
 const BANNER = document.getElementById('banner_image');
-
-//                                      ||   ASSISTANTS   ||                                      \\
-var avatar = new Assistant("program_assistant","/Images/Assistants/Programming/programGuy_neutral.png","300px", "400px", "82.5%");
+let avatar_Tom = ASSISTANT_LIST.Tom;
 
 //                                      ||   SUBCOMPONENTS   ||                                      \\
+let assistantName_SC = new Subcomponent("h3", "assistant_name", "assist-name");
 let chatBox_SC = new Subcomponent("div", "chat_box", "chat-box");
     let chatBoxMsg_SC = new Subcomponent("h2", "chat_message", "chat-msg");
 let whiteChatDiv_SC = new Subcomponent("div", "white_chatDiv", "white-chat");
     let blackChatBox_SC = new Subcomponent("div", "black_chatBox", "b-chat-box");
-        let blackChatBoxMsg_SC = new Subcomponent("h2", "b_chat_message", "b-chat-msg");
 
 
 export function initShowTime(){
+    // console.log(localStorage.getItem("assistantHasAppeared"));
+    if(localStorage.getItem("assistantHasAppeared") == null){
+        localStorage.setItem("assistantHasAppeared", true)
+    }
+    let storedValue = localStorage.getItem("assistantHasAppeared");
+    // console.log(localStorage.getItem("assistantHasAppeared"));
+    console.log(storedValue);
     hideElements()
     .then(async ()=>{
         $(document.body).css("background-image", "url('https://noware.tech/wp-content/uploads/sites/140/2018/10/bluescreen-t-1024x538.jpg')")
@@ -29,8 +35,8 @@ export function initShowTime(){
         await pause(500);
 
         // ASSISTANT ARRIVES
-        $(document.body).append(avatar.generate());
-        avatar.display("60%");
+        $(document.body).append(avatar_Tom.generate());
+        avatar_Tom.display("60%");
         $('#primary_container').append(chatBox_SC.createElement());
             $('#' + chatBox_SC.id).css("display", "none");
         await pause(1000);
@@ -39,50 +45,57 @@ export function initShowTime(){
         $('#' + chatBox_SC.id).append(chatBoxMsg_SC.createElement());
 
         // BEGINNING OF DIALOGUE
-        avatar.switchExpression("/Images/Assistants/Programming/programGuy_shocked.png")
-        .then(()=>typeMessage("chat_message", "W-What?!"))
+        avatar_Tom.switchExpression("shocked")
+        .then(()=>avatar_Tom.shake())
+        .then(async()=>{
+            await pause(250);
+            typeMessage("W-What?!")
+            await pause(500);
+        })
         .then(async ()=>{
-            await pause(500);
+            await pause(750);
             $('#chat_message').text("");
         })
-        .then(()=>avatar.switchExpression("/Images/Assistants/Programming/programGuy_neutral.png"))
-        .then(()=>typeMessage("chat_message", "Well that's not good..."))
-        .then(async()=>{
-            await pause(500);
-            $('#chat_message').text("");
-        })
-        .then(()=>avatar.switchExpression("/Images/Assistants/Programming/programGuy_smileOpen.png"))
-        .then(()=>typeMessage("chat_message", "Well this is kind of awkward!"))
-        .then(async()=>{
-            await pause(500);
-            $('#chat_message').text("");
-        })
-        .then(()=>typeMessage("chat_message", "I'm actually kind of new here, but the other devs had their hands full"))
-        .then(async()=>{
-            await pause(500);
-            $('#chat_message').text("");
-        })
-        .then(()=>avatar.switchExpression("/Images/Assistants/Programming/programGuy_shocked.png"))
-        .then(()=>typeMessage("chat_message", "Wait...  I wonder if..."))
-        .then(async()=>{
-            await pause(500);
-            $('#chat_message').text("");
-            await pause(1000);
-        })
-        .then(()=>avatar.switchExpression("/Images/Assistants/Programming/programGuy_neutral.png"))
-        .then(()=>typeMessage("chat_message", "Hold on, I'll be right back!"))
+        // .then(()=>avatar_Tom.switchExpression("default"))
+        // .then(()=>typeMessage("Well that's not good..."))
+        // .then(async()=>{
+        //     await pause(500);
+        //     $('#chat_message').text("");
+        // })
+        // .then(()=>avatar_Tom.switchExpression("open"))
+        // .then(()=>typeMessage("Well this is kind of awkward!"))
+        // .then(async()=>{
+        //     await pause(500);
+        //     $('#chat_message').text("");
+        // })
+        // .then(()=>typeMessage("I'm actually kind of new here, but the other devs had their hands full"))
+        // .then(async()=>{
+        //     await pause(500);
+        //     $('#chat_message').text("");
+        // })
+        // .then(()=>avatar_Tom.switchExpression("shocked"))
+        // .then(()=>avatar_Tom.shake())
+        // .then(()=>typeMessage("Wait...  I wonder if..."))
+        // .then(async()=>{
+        //     await pause(500);
+        //     $('#chat_message').text("");
+        //     await pause(500);
+        // })
+        .then(()=>avatar_Tom.switchExpression("default"))
+        .then(()=>typeMessage("Hold on, I'll be right back!"))
         .then(async()=>{
             await pause(500);
             $('#chat_message').text("");
             $('#' + chatBox_SC.id).fadeOut(1000);
             await pause(1000);
-            $('#' + avatar.id).animate({right: "125%"}, 1000);
+            $('#' + chatBox_SC.id).remove();
+            $('#' + avatar_Tom.id).animate({right: "125%"}, 1000);
             await pause(1000);
-            $('#' + avatar.id).remove();
+            $('#' + avatar_Tom.id).remove();
             $('#primary_container').animate({top: "1000px"}, 500);
-            await pause(1500);
+            await pause(1000);
             $(document.body).css("background-image", "url('https://miro.medium.com/max/1200/1*NXhs15W3eBIuLT9xWFXLcA.jpeg')");
-            await pause(5000);
+            await pause(1000);
             $(document.body).css("background-image", "url('https://www.thoughtco.com/thmb/mvmMSSC5cnBtqNmQ_hfFSTD7yew=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/css-code-in-text-editor--web-page-internet-technology-862672426-5c8455ddc9e77c0001a67650.jpg')");
             await pause(500);
         })
@@ -90,17 +103,43 @@ export function initShowTime(){
         .then(()=>{
             returnElements()
             .then(async()=>{
-                $(document.body).append(avatar.generate());
-                $('#' + avatar.id).css({"z-index": "3", top: "60%", right: "100%"});
-                $('#' + avatar.id).animate({right: "82.5%"}, 1000)
+                $(document.body).append(avatar_Tom.generate());
+                $('#' + avatar_Tom.id).css({"z-index": "3", top: "60%", right: "100%"});
+                $('#' + avatar_Tom.id).animate({right: "82.5%"}, 1000)
                 await pause(1000);
+                $('#' + whiteChatDiv_SC.id).css("display", "flex");
+                $('#' + whiteChatDiv_SC.id).animate({top: "625px"}, 500)
+                await pause(500);
+                $('#' + blackChatBox_SC.id).fadeIn(750);
+                await pause(750);
             })
-            .then(()=>avatar.switchExpression("/Images/Assistants/Programming/programGuy_smile.png"))
-            .then(()=>typeMessage("b_chat_message", "Wow, I can't believe that actually worked."))
+            .then(()=>avatar_Tom.switchExpression("shocked"))
+            .then(()=>typeMessage("Wow, I can't believe that actually worked."))
             .then(async()=>{
                 await pause(500);
-                $('#b_chat_message').text("");
+                $('#chat_message').text("");
             })
+            // .then(()=>avatar_Tom.switchExpression("open"))
+            // .then(()=>typeMessage("All I did was turn it off and turn it back on!  Can you believe that?"))
+            // .then(async()=>{
+            //     await pause(500);
+            //     $('#chat_message').text("");
+            // })
+            .then(()=>avatar_Tom.switchExpression("smile"))
+            .then(()=>typeMessage("Anyway, hello there!  My name's Tom."))
+            .then(async()=>{
+                await pause(500);
+                $('#chat_message').text("");
+            })
+            .then(async()=>{
+                $('#' + whiteChatDiv_SC.id).append(assistantName_SC.createElement([{attr:"",content:"",innerText:"Tom"}]));
+                $('#' + assistantName_SC.id).animate({left: "18.5%", opacity: 1}, 1000)
+                await pause(1000);
+            })
+            .then(()=>avatar_Tom.switchExpression("open"))
+            .then(()=>typeMessage("It's a pleasure to meet you!"))
+            .then(()=>avatar_Tom.switchExpression("smile"))
+            .then(()=>firstReply());
         })
     })
     // RE-ENABLE WEBPAGE SCROLLING
@@ -134,14 +173,14 @@ function hideElements(){
     })
 }
 
-function typeMessage(box, msg){
+function typeMessage(msg){
     return new Promise(resolve=>{
         let x = 0;
         let chatArr = msg.split("");
         chatArr.join("");
         let splitByLetter = function(){
             if(x < chatArr.length){
-                $('#' + box).append(chatArr[x])
+                $('#chat_message').append(chatArr[x])
                 x++;
             } else {
                 clearInterval(myInterval);
@@ -169,13 +208,9 @@ function returnElements(){
 
         $(document.body).prepend(whiteChatDiv_SC.createElement());
             $('#' + whiteChatDiv_SC.id).append(blackChatBox_SC.createElement());
-                $('#' + blackChatBox_SC.id).append(blackChatBoxMsg_SC.createElement());
-
-        $('#' + whiteChatDiv_SC.id).css("display", "flex")
+                $('#' + blackChatBox_SC.id).append(chatBoxMsg_SC.createElement());
+        $('#' + whiteChatDiv_SC.id).css("display", "none")
             $('#' + blackChatBox_SC.id).css("display", "none");
-        $('#' + whiteChatDiv_SC.id).animate({top: "625px"}, 1000)
-            $('#' + blackChatBox_SC.id).fadeIn(1000);
-        await pause(1000);
         resolve();
     })
 }
