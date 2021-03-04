d1dc847e3e748c44bfdc509d147dfec0d3350e42
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-03 22:38:10 -0500

    Update main.games.js

--- Quizlet Clone/Games/main.games.js
+++ Quizlet Clone/Games/main.games.js
@@ -2,6 +2,6 @@ import { startGameOne } from "./firstGame.js";
 import { startGameTwo } from "./secondGame.js";
 
 $(document).ready(()=>{
-    startGameOne();
-    // startGameTwo();
+    // startGameOne();
+    startGameTwo();
 });
