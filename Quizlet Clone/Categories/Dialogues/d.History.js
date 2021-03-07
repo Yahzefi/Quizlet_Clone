31932c6efb2948d7c6662faaeeb1add8a6a57b27
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-07 00:18:24 -0500

    Update d.History.js

--- Quizlet Clone/Categories/Dialogues/d.History.js
+++ Quizlet Clone/Categories/Dialogues/d.History.js
@@ -1,2 +1,147 @@
 // import { toggleDialogueBox } from "../../Initialization/Assistants.js";
 // DIALOGUE PATHS WITH HISTORY ASSISTANT
+
+export function nextLine_Hist(branchNum, d_Num, responseChar){
+    return new Promise((resolve)=>{
+        let logUpdate;
+        let introScript = function(){
+            if(responseChar === "A"){
+                switch(d_Num){
+                    case 0:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    case 1:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    default:
+                        throw new Error("Line Not Found")
+                }
+            } else if(responseChar === "B"){
+                switch(d_Num){
+                    case 0:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    case 1:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    default:
+                        throw new Error("Line Not Found")
+                }
+            } else if(responseChar === "C"){
+                switch(d_Num){
+                    case 0:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    case 1:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    default:
+                        throw new Error("Line Not Found")
+                }
+            }
+        }
+        let firstBranch = function(){
+            if(responseChar === "A"){
+                switch(d_Num){
+                    case 0:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    case 1:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    default:
+                        throw new Error("Line Not Found")
+                }
+            } else if(responseChar === "B"){
+                switch(d_Num){
+                    case 0:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    case 1:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    default:
+                        throw new Error("Line Not Found")
+                }
+            } else if(responseChar === "C"){
+                switch(d_Num){
+                    case 0:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    case 1:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    default:
+                        throw new Error("Line Not Found")
+                }
+            }
+        }
+        let secondBranch = function(){
+            if(responseChar === "A"){
+                switch(d_Num){
+                    case 0:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    case 1:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    default:
+                        throw new Error("Line Not Found")
+                }
+            } else if(responseChar === "B"){
+                switch(d_Num){
+                    case 0:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    case 1:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    default:
+                        throw new Error("Line Not Found")
+                }
+            } else if(responseChar === "C"){
+                switch(d_Num){
+                    case 0:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    case 1:
+                        logUpdate = null
+                        resolve(logUpdate)
+                        break;
+                    default:
+                        throw new Error("Line Not Found")
+                }
+            }
+        }
+        switch(branchNum){
+            case 0:
+                introScript();
+                break;
+            case 1:
+                firstBranch();
+                break;
+            case 2:
+                secondBranch();
+                break;
+            default:
+                throw new Error("No Branch Found");
+        }
+    })
+}
