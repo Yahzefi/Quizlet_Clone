68d2f1a10340f390c3b73526b61238564a98887c
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-13 01:24:51 -0500

    Add files via upload

---
+++ Quizlet Clone/Categories/c_Math/showtime.math.js
@@ -0,0 +1,10 @@
+import { pause } from "../c_Redirect.js"
+import { ASSISTANT_LIST } from '../../Initialization/on_Init.js'
+
+export async function initShowTime(){
+    $('#body_container').css('display', 'none');
+    // Extend Primary container && then animate it to slide upwards
+    //Reveal Assistant
+    let avatar_Math = ASSISTANT_LIST.Kinsley;
+    
+}
