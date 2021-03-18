51116525c735b42991c99b096486b2016761d359
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-17 22:48:10 -0400

    Update d_span.script.js

--- Quizlet Clone/Categories/Dialogues/Spanish/d_span.script.js
+++ Quizlet Clone/Categories/Dialogues/Spanish/d_span.script.js
@@ -5,52 +5,24 @@ export function nextLine_Span(branchNum, d_Num, responseChar){
     return new Promise((resolve)=>{
         let logUpdate;
         let introScript = function(){
-            if(responseChar === "A"){
-                switch(d_Num){
-                    case 0:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    case 1:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    default:
-                        throw new Error("Line Not Found")
-                }
-            } else if(responseChar === "B"){
-                switch(d_Num){
-                    case 0:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    case 1:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    default:
-                        throw new Error("Line Not Found")
-                }
-            } else if(responseChar === "C"){
-                switch(d_Num){
-                    case 0:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    case 1:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    default:
-                        throw new Error("Line Not Found")
-                }
+            switch(d_Num){
+                case 0:
+                    console.log("Spanish");
+                    $('#dChoice_A').text("\"It's nice to meet you too!\"");
+                    $('#dChoice_B').text("\"Uh...\"");
+                    $('#dChoice_C').text("\"Please leave me alone...\"");
+                    logUpdate = 1;
+                    resolve(logUpdate);
+                    break;
+                default:
+                    throw new Error("Line Not Found");
             }
         }
         let firstBranch = function(){
             if(responseChar === "A"){
                 switch(d_Num){
                     case 0:
-                        logUpdate = null
+                        logUpdate = -1
                         resolve(logUpdate)
                         break;
                     case 1:
