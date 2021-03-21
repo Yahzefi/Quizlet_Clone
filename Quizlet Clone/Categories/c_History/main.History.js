bf870ccbf8f9c13a784cc79c8d5ed92aec9d989a
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-21 01:48:19 -0400

    Update main.History.js

--- Quizlet Clone/Categories/c_History/main.History.js
+++ Quizlet Clone/Categories/c_History/main.History.js
@@ -6,28 +6,47 @@ $(document).ready(()=>{$('#primary_container').fadeIn(500)})
 
 let bookPulse;
 
+// $('#history_startBook').hover(function(){
+//     $(this).animate({width: "300px", height: "400px"}, 500);
+//     bookPulse = setInterval(()=>{
+//         $('#history_startBook').animate({opacity: "0"},600)
+//         $('#history_startBook').animate({opacity: "1"},600)
+//     }, 1200)
+// }, function(){
+//     clearInterval(bookPulse);
+//     $(this).animate({width: "225px", height: "300px"}, 500);
+// })
+
+// $('#history_startBook').click(async function(){
+//     await pause(250);
+//     $(this).animate({width: "500px", height:"550px"}, 750)
+//     $(this).animate({width: "0px", height: "0px"}, 500)
+//     $(this).animate({opacity: "0"}, 250)
+//     await pause(2000)
+//     $(this).remove();
+//     $('#start_message').animate({fontSize: "128px"}, 500)
+//     $('#start_message').animate({fontSize: "0px"}, 500)
+//     await pause(1000);
+//     $('#start_message').remove();
+//     await pause(250)
+//     initShowTime();
+// })
+
 $('#history_startBook').hover(function(){
-    $(this).animate({width: "300px", height: "400px"}, 500);
-    bookPulse = setInterval(()=>{
-        $('#history_startBook').animate({opacity: "0"},600)
-        $('#history_startBook').animate({opacity: "1"},600)
-    }, 1200)
+    this.style.animationName = "shadowPulse";
+    this.style.animationDuration = "1.5s";
+    this.style.animationIterationCount = 'infinite';
 }, function(){
-    clearInterval(bookPulse);
-    $(this).animate({width: "225px", height: "300px"}, 500);
+    this.style.animation = "";
 })
 
 $('#history_startBook').click(async function(){
-    await pause(250);
-    $(this).animate({width: "500px", height:"550px"}, 750)
-    $(this).animate({width: "0px", height: "0px"}, 500)
-    $(this).animate({opacity: "0"}, 250)
-    await pause(2000)
+    $(this).fadeOut(500);
+    $('#body_container h2').animate({fontSize: "84px"}, 500)
+    await pause(500);
     $(this).remove();
-    $('#start_message').animate({fontSize: "128px"}, 500)
-    $('#start_message').animate({fontSize: "0px"}, 500)
-    await pause(1000);
-    $('#start_message').remove();
-    await pause(250)
+    $('#body_container h2').animate({fontSize: "0px"}, 500)
+    await pause(500);
+    $('#body_container h2').remove();
     initShowTime();
 })
