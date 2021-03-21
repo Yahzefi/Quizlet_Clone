f0a3aa3a0ca32bc6aefcdbed7f26c3319f645769
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-21 01:51:49 -0400

    Update d_span.script.js

--- Quizlet Clone/Categories/Dialogues/Spanish/d_span.script.js
+++ Quizlet Clone/Categories/Dialogues/Spanish/d_span.script.js
@@ -1,5 +1,8 @@
-// import { toggleDialogueBox } from "../../Initialization/Assistants.js";
-// DIALOGUE PATHS WITH SPANISH ASSISTANT
+import { typeMessage, dialogueLB } from "../../../Initialization/Assistants.js"
+import { pause } from "../../c_Redirect.js"
+import { ASSISTANT_LIST } from "../../../Initialization/on_Init.js"
+
+const avatar = ASSISTANT_LIST.Damien;
 
 export function nextLine_Span(branchNum, d_Num, responseChar){
     return new Promise((resolve)=>{
@@ -8,9 +11,9 @@ export function nextLine_Span(branchNum, d_Num, responseChar){
             switch(d_Num){
                 case 0:
                     console.log("Spanish");
-                    $('#dChoice_A').text("\"It's nice to meet you too!\"");
-                    $('#dChoice_B').text("\"Uh...\"");
-                    $('#dChoice_C').text("\"Please leave me alone...\"");
+                    $('#dChoice_A').text("\"It seemed like the easiest option.\"");
+                    $('#dChoice_B').text("\"I flipped a coin.\"");
+                    $('#dChoice_C').text("\"Can't we skip the icebreakers?\"");
                     logUpdate = 1;
                     resolve(logUpdate);
                     break;
@@ -22,11 +25,40 @@ export function nextLine_Span(branchNum, d_Num, responseChar){
             if(responseChar === "A"){
                 switch(d_Num){
                     case 0:
-                        logUpdate = -1
-                        resolve(logUpdate)
+                        avatar.switchExpression("open")
+                        .then(()=>typeMessage("Is that so?"))
+                        .then(async()=>await dialogueLB())
+                        .then(()=>avatar.switchExpression("smile"))
+                        .then(()=>typeMessage("I suppose Spanish is known as one of the simpler languages."))
+                        .then(async()=>await dialogueLB())
+                        .then(()=>typeMessage("Sometimes that lower learning curve helps out quite a bit!"))
+                        .then(async()=>await dialogueLB())
+                        .then(()=>typeMessage("It won't be easy though, but I hope you won't get discouraged!"))
+                        .then(async()=>await dialogueLB())
+                        .then(()=>typeMessage("After all, learning a second language is an amazing skill to acquire."))
+                        .then(async()=>await dialogueLB())
+                        .then(()=>typeMessage("I can attest to that personally, too!"))
+                        .then(async()=>await dialogueLB())
+                        .then(()=>avatar.switchExpression("open"))
+                        .then(()=>typeMessage("I can't imagine my life without it!"))
+                        .then(async()=>await dialogueLB())
+                        .then(()=>avatar.switchExpression("smile"))
+                        .then(()=>typeMessage("Well I think I've talked yoru ear off enough, yes?"))
+                        .then(async()=>await dialogueLB())
+                        .then(()=>typeMessage("Let's get started!"))
+                        .then(async()=>await dialogueLB())
+                        .then(()=>{
+                            logUpdate = 0
+                            resolve(logUpdate)
+                        })
                         break;
                     case 1:
-                        logUpdate = null
+                        $('#d_instructions').text("");
+                        $('#dChoice_C').css({fontSize: "54px", letterSpacing: "10px", position: "relative", top: "20%", left: "0%"})
+                        $('#dChoice_A').text("");
+                        $('#dChoice_B').text("");
+                        $('#dChoice_C').text("Click to Continue");
+                        logUpdate = 1
                         resolve(logUpdate)
                         break;
                     default:
@@ -35,11 +67,31 @@ export function nextLine_Span(branchNum, d_Num, responseChar){
             } else if(responseChar === "B"){
                 switch(d_Num){
                     case 0:
-                        logUpdate = null
-                        resolve(logUpdate)
+                        avatar.switchExpression("shocked")
+                        .then(()=>typeMessage("W-Wait, really?"))
+                        .then(async()=>{await dialogueLB()})
+                        .then(()=>avatar.switchExpression("default"))
+                        .then(()=>typeMessage("That's it?"))
+                        .then(async()=>{await dialogueLB()})
+                        .then(()=>avatar.switchExpression("smile"))
+                        .then(()=>typeMessage("Well I suppose it could be worse."))
+                        .then(async()=>{await dialogueLB()})
+                        .then(()=>typeMessage("As long as you're willing to learn, you're in the right place!"))
+                        .then(async()=>{await dialogueLB()})
+                        .then(()=>typeMessage("So, shall we begin?"))
+                        .then(async()=>{await dialogueLB()})
+                        .then(()=>{
+                            logUpdate = 0
+                            resolve(logUpdate)
+                        })
                         break;
                     case 1:
-                        logUpdate = null
+                        $('#d_instructions').text("");
+                        $('#dChoice_C').css({fontSize: "54px", letterSpacing: "10px", position: "relative", top: "20%", left: "0%"})
+                        $('#dChoice_A').text("");
+                        $('#dChoice_B').text("");
+                        $('#dChoice_C').text("Click to Continue");
+                        logUpdate = 1
                         resolve(logUpdate)
                         break;
                     default:
@@ -48,11 +100,28 @@ export function nextLine_Span(branchNum, d_Num, responseChar){
             } else if(responseChar === "C"){
                 switch(d_Num){
                     case 0:
-                        logUpdate = null
-                        resolve(logUpdate)
+                        avatar.switchExpression("shocked")
+                        .then(()=>typeMessage("O-Oh, um...  Yes, of course."))
+                        .then(async()=>{await dialogueLB()})
+                        .then(()=>avatar.switchExpression("default"))
+                        .then(()=>typeMessage("If you change your mind, you can click on the icon at the top left of the screen."))
+                        .then(async()=>{await dialogueLB()})
+                        .then(()=>avatar.switchExpression("open"))
+                        .then(()=>typeMessage("Good luck with your studies!"))
+                        .then(async()=>{await dialogueLB()})
+                        .then(()=>avatar.switchExpression("smile"))
+                        .then(()=>{
+                            logUpdate = 0
+                            resolve(logUpdate)
+                        })
                         break;
                     case 1:
-                        logUpdate = null
+                        $('#d_instructions').text("");
+                        $('#dChoice_C').css({fontSize: "54px", letterSpacing: "10px", position: "relative", top: "20%", left: "0%"})
+                        $('#dChoice_A').text("");
+                        $('#dChoice_B').text("");
+                        $('#dChoice_C').text("Click to Continue");
+                        logUpdate = 1
                         resolve(logUpdate)
                         break;
                     default:
@@ -61,45 +130,13 @@ export function nextLine_Span(branchNum, d_Num, responseChar){
             }
         }
         let secondBranch = function(){
-            if(responseChar === "A"){
-                switch(d_Num){
-                    case 0:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    case 1:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    default:
-                        throw new Error("Line Not Found")
-                }
-            } else if(responseChar === "B"){
-                switch(d_Num){
-                    case 0:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    case 1:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    default:
-                        throw new Error("Line Not Found")
-                }
-            } else if(responseChar === "C"){
-                switch(d_Num){
-                    case 0:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    case 1:
-                        logUpdate = null
-                        resolve(logUpdate)
-                        break;
-                    default:
-                        throw new Error("Line Not Found")
-                }
+            switch(d_Num){
+                case 0:
+                    logUpdate = -1;
+                    resolve(logUpdate);
+                    break;
+                default:
+                    throw Error("Line Not Found");
             }
         }
         switch(branchNum){
