c86cca0dffce6524f64c306fade74cbaedbfa793
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-21 01:50:36 -0400

    Update main.Spanish.js

--- Quizlet Clone/Categories/c_Spanish/main.Spanish.js
+++ Quizlet Clone/Categories/c_Spanish/main.Spanish.js
@@ -1,12 +1,20 @@
 import { pause } from "../c_Redirect.js"
+import { initShowTime } from "../c_Spanish/showtime.span.js"
 
 
 $('#c_tortilla').click(async function(){
     $('#body_container h4').fadeOut(500);
-    await pause(750)
-    $(this).animate({left: "1500px"}, 1000)
+    $('#body_container h1').fadeOut(500);
+    $('#body_container h2').fadeOut(500);
+    await pause(500)
+    $('#f_head').fadeOut(500);
     await pause(1000);
+    $(this).animate({width: "300px", height: "300px"}, 500)
+    $(this).animate({width: "0px", height: "0px"}, 1000)
+    await pause(1500);
+    $('#c_head').remove();
     $(this).remove();
+    initShowTime();
 })
 
 $('#f_tortilla').click(async function(){
