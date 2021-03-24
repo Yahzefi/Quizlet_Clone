2744fef73235d9fb73c3f96687f4c7a050001901
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-24 11:07:16 -0400

    Update d_prog.script.js

--- Quizlet Clone/Categories/Dialogues/Programming/d_prog.script.js
+++ Quizlet Clone/Categories/Dialogues/Programming/d_prog.script.js
@@ -41,18 +41,9 @@ export function nextLine_Prog(branchNum, d_Num, responseChar){
                         .then(()=>typeMessage("Are you ready to start coding?"))
                         .then(async()=>{await dialogueLB();})
                         .then(()=>{
-                            logUpdate = 0;
+                            logUpdate = -1;
                             resolve(logUpdate);
                         })
-                        break;
-                    case 2:
-                        $('#d_instructions').text("");
-                        $('#dChoice_C').css({fontSize: "54px", letterSpacing: "10px", position: "relative", top: "20%", left: "0%"})
-                        $('#dChoice_A').text("");
-                        $('#dChoice_B').text("");
-                        $('#dChoice_C').text("Click to Continue");
-                        logUpdate = 1;
-                        resolve(logUpdate);
                         break;
                     default:
                         throw new Error("Line Not Found.")
@@ -63,29 +54,20 @@ export function nextLine_Prog(branchNum, d_Num, responseChar){
                         avatar.switchExpression("shocked")
                         .then(()=>typeMessage("Oh gosh, d-did I break you?"))
                         .then(async()=>{await dialogueLB()})
-                        .then(()=>typeMessage("Are you broken?"))
-                        .then(async()=>{await dialogueLB()})
-                        .then(()=>avatar.switchExpression("open"))
-                        .then(()=>typeMessage("Don't worry, I can fix this."))
-                        .then(async()=>{await dialogueLB()})
-                        .then(()=>typeMessage("All you need is the awesome power of knowledge, right?"))
-                        .then(async()=>{await dialogueLB()})
-                        .then(()=>avatar.switchExpression("smile"))
-                        .then(()=>typeMessage("So then, let's begin!"))
-                        .then(async()=>{await dialogueLB()})
+                        // .then(()=>typeMessage("Are you broken?"))
+                        // .then(async()=>{await dialogueLB()})
+                        // .then(()=>avatar.switchExpression("open"))
+                        // .then(()=>typeMessage("Don't worry, I can fix this."))
+                        // .then(async()=>{await dialogueLB()})
+                        // .then(()=>typeMessage("All you need is the awesome power of knowledge, right?"))
+                        // .then(async()=>{await dialogueLB()})
+                        // .then(()=>avatar.switchExpression("smile"))
+                        // .then(()=>typeMessage("So then, let's begin!"))
+                        // .then(async()=>{await dialogueLB()})
                         .then(()=>{
-                            logUpdate = 0
+                            logUpdate = -1
                             resolve(logUpdate)
                         })
-                        break;
-                    case 1:
-                        $('#d_instructions').text("");
-                        $('#dChoice_C').css({fontSize: "54px", letterSpacing: "10px", position: "relative", top: "20%", left: "0%"})
-                        $('#dChoice_A').text("");
-                        $('#dChoice_B').text("");
-                        $('#dChoice_C').text("Click to Continue");
-                        logUpdate = 1;
-                        resolve(logUpdate);
                         break;
                     default:
                         throw new Error("Line Not Found")
@@ -103,35 +85,22 @@ export function nextLine_Prog(branchNum, d_Num, responseChar){
                         .then(async()=>{await dialogueLB()})
                         .then(()=>typeMessage("If you change your mind, click the icon at the top left of the screen."))
                         .then(async()=>{await dialogueLB()})
+                        .then(async()=>{
+                            $('#assistant_help').fadeIn(1000);
+                            await pause(1000);
+                        })
                         .then(()=>typeMessage("Good luck on your journey!"))
                         .then(async()=>{await dialogueLB()})
+                        .then(async()=>{$('#' + avatar.id).animate({right: "100%"}, 1000);await pause(1000);$('#' + avatar.id).remove()})
+                        .then(async()=>{$('#chat_div').fadeOut(1000);await pause(1000)})
                         .then(()=>{
-                            logUpdate = 0;
+                            logUpdate = -1;
                             resolve(logUpdate);
                         })
-                        break;
-                    case 1:
-                        $('#d_instructions').text("");
-                        $('#dChoice_C').css({fontSize: "54px", letterSpacing: "10px", position: "relative", top: "20%", left: "0%"})
-                        $('#dChoice_A').text("");
-                        $('#dChoice_B').text("");
-                        $('#dChoice_C').text("Click to Continue");
-                        logUpdate = 1;
-                        resolve(logUpdate);
                         break;
                     default:
                         throw new Error("Line Not Found")
                 }
-            }
-        }
-        let secondBranch = function(){
-            switch(d_Num){
-                case 0:
-                    logUpdate = -1
-                    resolve(logUpdate)
-                    break;
-                default:
-                    throw Error("Line Not Found");
             }
         }
         switch(branchNum){
@@ -141,11 +110,82 @@ export function nextLine_Prog(branchNum, d_Num, responseChar){
             case 1:
                 firstBranch();
                 break;
-            case 2:
-                secondBranch();
-                break;
             default:
                 throw new Error("No Branch Found");
+        }
+    })
+}
+
+export function pageIntroduction_Programming(lineNumber){
+    return new Promise((resolve)=>{
+        switch(lineNumber){
+            case 0:
+                avatar.switchExpression("open")
+                .then(()=>typeMessage("With the first section, we decided to start with the basics."))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>avatar.switchExpression("smile"))
+                .then(()=>typeMessage("If you're interested in Web Development, it's important to understand the skeleton before you can start dissecting the muscles and other bits."))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>resolve());
+                break;
+            case 1:
+                avatar.switchExpression("open")
+                .then(()=>typeMessage("The second section covers the styling of the site.  CSS stands for Cascading Style Sheets after all!"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>avatar.switchExpression("smile"))
+                .then(()=>typeMessage("If you want to focus on front-end development, it's a must to understand the concepts revolving around CSS."))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>resolve());
+                break;
+            case 2:
+                avatar.switchExpression("open")
+                .then(()=>typeMessage("Alright, alright...  This is the big kahuna!"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>avatar.switchExpression("smile"))
+                .then(()=>typeMessage("Once you've understood the basics of HTML and CSS, you'll most likely find yourself moving towards the functionality of it all"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>typeMessage("JavaScript allows you to really stick your foot in the door of creating a webiste or other applications."))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>resolve());
+                break;
+            case 3:
+                avatar.switchExpression("open")
+                .then(()=>typeMessage("The fourth section has to do with the JavaScript framework, \"React\""))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>avatar.switchExpression("smile"))
+                .then(()=>typeMessage("A framework or library is designed to allow the user to code within a certain structure to allow for more organized and collaboration-friendly code."))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>resolve());
+                break;
+            case 4:
+                avatar.switchExpression("open")
+                .then(()=>typeMessage("The last section is Node.js"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>avatar.switchExpression("smile"))
+                .then(()=>typeMessage("Node focuses on the back-end development side of things, and allows communication with a server to access various types of information"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>typeMessage("I'm still a little fuzzy on these kinds of things, but from what I've seen so far it's just as fascinating as everything else!"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>resolve());
+                break;
+            case 5:
+                avatar.switchExpression("open")
+                .then(()=>typeMessage("And the next thing...  Oh wait!"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>avatar.switchExpression("smile"))
+                .then(()=>typeMessage("I think that's it!"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>typeMessage("Wow, that went by fast didn't it?"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>typeMessage("If you require any additional support, just click the icon at the top left of the screen."))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>$('#assistant_help').fadeIn(500))
+                .then(()=>typeMessage("Good Luck!"))
+                .then(async()=>{await dialogueLB()})
+                .then(()=>resolve());
+                break;
+            default:
+                throw Error("Line Not Found");
         }
     })
 }
