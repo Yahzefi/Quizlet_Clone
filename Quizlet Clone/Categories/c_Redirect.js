feb5cd70f354dfe7dead96495d1be7e5d8d4dca8
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-23 22:17:04 -0400

    Update c_Redirect.js

--- Quizlet Clone/Categories/c_Redirect.js
+++ Quizlet Clone/Categories/c_Redirect.js
@@ -1,3 +1,11 @@
+import { anchorRedirect_Math } from "./c_Math/main.Math.js";
+import { anchorRedirect_History } from "./c_History/main.History.js";
+import { anchorRedirect_Spanish } from "./c_Spanish/main.Spanish.js";
+
+import { pageIntroduction_History } from "./Dialogues/History/d_hist.script.js";
+import { pageIntroduction_Math } from "./Dialogues/Math/d_math.script.js";
+import { pageIntroduction_Spanish } from "./Dialogues/Spanish/d_span.script.js";
+
 export function openMathPage(){
     window.open("/Categories/c_Math/index.Math.html");
 }
@@ -18,4 +26,37 @@ export function pause(ms){
     return new Promise((resolve)=>{
         setTimeout(resolve, ms);
     })
+}
+
+export function pageIntroduction(category, lineNumber){
+    return new Promise((resolve)=>{
+        if(category === "math"){
+            pageIntroduction_Math(lineNumber)
+            .then(()=>resolve())
+        } else if(category === "history"){
+            pageIntroduction_History(lineNumber)
+            .then(()=>resolve())
+        } else if(category === "spanish"){
+            pageIntroduction_Spanish(lineNumber)
+            .then(()=>resolve())
+        } else if(category === "programming"){
+    
+        }
+    })
+}
+
+export function anchorRedirect(ev, category){
+    switch(category){
+        case "math":
+            anchorRedirect_Math(ev.target.id)
+            break;
+        case "history":
+            anchorRedirect_History(ev.target.id);
+            break;
+        case "spanish":
+            anchorRedirect_Spanish(ev.target.id);
+            break;
+        default:
+            throw Error("Error: Redirect Failed")
+    }
 }
