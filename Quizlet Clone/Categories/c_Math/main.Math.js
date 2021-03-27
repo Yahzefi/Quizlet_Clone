104c45279b5e03dd42be245d2322bf688642825d
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-27 10:36:14 -0400

    Update main.Math.js

--- Quizlet Clone/Categories/c_Math/main.Math.js
+++ Quizlet Clone/Categories/c_Math/main.Math.js
@@ -21,5 +21,32 @@ $('#calculator_image').click(async function(){
     $('#body_container h2').animate({fontSize: "0px"}, 500)
     await pause(500);
     $('#body_container h2').remove();
+    $('#body_container').remove();
     initShowTime();
 })
+
+$('#assistant_help').on("click", ()=>{
+    console.log("help");
+})
+
+export function anchorRedirect_Math(selectedAnchor){
+    switch(selectedAnchor){
+        case "a1":
+            console.log("Basics")
+            break;
+        case "a2":
+            console.log("Algebra")
+            break;
+        case "a3":
+            console.log("Geometry")
+            break;
+        case "a4":
+            console.log("Calculus")
+            break;
+        case "a5":
+            console.log("Physics")
+            break;
+        default:
+            throw Error("Error 404: Page Not Found")
+    }
+}
