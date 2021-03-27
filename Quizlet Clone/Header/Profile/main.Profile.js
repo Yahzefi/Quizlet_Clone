c61a52df8a514baaf22c9e5717781c9c5c836679
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-27 10:46:59 -0400

    Update main.Profile.js

--- Quizlet Clone/Header/Profile/main.Profile.js
+++ Quizlet Clone/Header/Profile/main.Profile.js
@@ -1,1 +1,11 @@
-// Profile
+$('#home_span').click(()=>{
+    window.history.back();
+})
+
+$('#settings_span').click(()=>console.log("You clicked the \"Settings\" span"))
+
+$('#rec_act').click(()=>$('#content_section h2').text("Recent Activity"))
+$('#sub_set').click(()=>$('#content_section h2').text("Submitted Sets"))
+$('#fol_set').click(()=>$('#content_section h2').text("Followed Sets"))
+$('#course_subs').click(()=>$('#content_section h2').text("Course Subscriptions"))
+$('#over_stats').click(()=>$('#content_section h2').text("Overall Statistics"))
