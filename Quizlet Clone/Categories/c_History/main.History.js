0c53af98a5870144c79a0febba6ce08ee07e1120
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-27 10:35:15 -0400

    Update main.History.js

--- Quizlet Clone/Categories/c_History/main.History.js
+++ Quizlet Clone/Categories/c_History/main.History.js
@@ -1,49 +1,47 @@
-import { Subcomponent } from "../../Initialization/Subcomponent.js"
-import { ASSISTANT_LIST } from "../../Initialization/on_Init.js"
 import { pause } from "../c_Redirect.js"
-import { typeMessage, dialogueLB } from "../../Initialization/Assistants.js"
-import { beginConversation } from "../Dialogues/d.Main.js";
+import { initShowTime } from "./showtime.hist.js"
 
-let assistantName_SC = new Subcomponent("h3", "assistant_name", "assist-name");
-let chatDiv_SC = new Subcomponent("div", "chat_div", "chat-div");
-    let chatBox_SC = new Subcomponent("div", "chat_box", "chat-box");
-        let chatBoxMsg_SC = new Subcomponent("h2", "chat_message", "chat-msg");
+$('#primary_container').css("top", "0px")
+$(document).ready(()=>{$('#primary_container').fadeIn(500)})
 
-export async function initShowTime(){
-    let avatar_Fred = ASSISTANT_LIST.Fredrick;
-    $(document.body).append(avatar_Fred.generate());
-    avatar_Fred.display("60%");
-    await pause(1000);
-    $(document.body).prepend(chatDiv_SC.createElement());
-        $('#' + chatDiv_SC.id).append(chatBox_SC.createElement());
-            $('#' + chatBox_SC.id).append(chatBoxMsg_SC.createElement());
-            $('#' + chatBox_SC.id).css("display", "none");
-    $('#' + chatDiv_SC.id).animate({top:"625px"}, 1000)
-    await pause(1000);
-    $('#header_container').fadeIn(750);
-    await pause(750);
-    $('#' + chatBox_SC.id).fadeIn(750);
-    await pause(750);
-    avatar_Fred.switchExpression("default")
-    .then(()=>typeMessage("Hmm..."))
-    // .then(async ()=>{await dialogueLB()})
-    // .then(()=>avatar_Fred.switchExpression("shocked"))
-    // .then(()=>typeMessage("You must be a fellow historian, yes?"))
-    // .then(async()=>{await dialogueLB()})
-    // .then(()=>avatar_Fred.switchExpression("open"))
-    // .then(()=>avatar_Fred.shake())
-    // .then(async()=>{await pause(500)})
-    // .then(()=>typeMessage("Oh, no!  Where are my manners?"))
-    // .then(async()=>{await dialogueLB()})
-    // .then(()=>typeMessage("I'm Fredrick, it's a pleasure to make your aquaintance!"))
-    .then(async()=>{await dialogueLB()})
-    .then(()=>avatar_Fred.switchExpression("smile"))
-    .then(async ()=>{
-        $('#chat_div').prepend(assistantName_SC.createElement([{attr:"", content:"", innerText:"Fredrick"}]))
-        $('#' + assistantName_SC.id).animate({left: "18%", opacity: 1}, 1000)
-        await pause(850);
-        $('#' + assistantName_SC.id).css("font-style", "normal")
-        await pause(150);
-    })
-    .then(()=>beginConversation("H"))
+$('#history_startBook').hover(function(){
+    this.style.animationName = "shadowPulse";
+    this.style.animationDuration = "1.5s";
+    this.style.animationIterationCount = 'infinite';
+}, function(){
+    this.style.animation = "";
+})
+
+$('#history_startBook').click(async function(){
+    $(this).fadeOut(500);
+    $('#body_container h2').animate({fontSize: "84px"}, 500)
+    await pause(500);
+    $(this).remove();
+    $('#body_container h2').animate({fontSize: "0px"}, 500)
+    await pause(500);
+    $('#body_container h2').remove();
+    $('#body_container').remove();
+    initShowTime();
+})
+
+export function anchorRedirect_History(selectedAnchor){
+    switch(selectedAnchor){
+        case "a1":
+            alert("North America")
+            break;
+        case "a2":
+            alert("South America")
+            break;
+        case "a3":
+            alert("Africa")
+            break;
+        case "a4":
+            alert("Australia")
+            break;
+        case "a5":
+            alert("Europe")
+            break;
+        default:
+            throw Error("Error 404: Page Not Found")
+    }
 }
