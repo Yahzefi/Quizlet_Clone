e0e802650ca6df569b984b23181c4e6401ee91ef
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-07 21:37:00 -0400

    Update main.Math.js

--- Quizlet Clone/Categories/c_Math/main.Math.js
+++ Quizlet Clone/Categories/c_Math/main.Math.js
@@ -1,6 +1,8 @@
 import { pause } from "../c_Redirect.js"
 import { initShowTime } from "./showtime.math.js"
 
+// Starting Intro Logo Animation & Handling
+
 $(document).ready(()=>{
     $('#body_container').css("margin", "15% auto")
 })
@@ -25,9 +27,13 @@ $('#calculator_image').click(async function(){
     initShowTime();
 })
 
+// Help Icon Functionality
+
 $('#assistant_help').on("click", ()=>{
     console.log("help");
 })
+
+// Navigational Redirect Function
 
 export function anchorRedirect_Math(selectedAnchor){
     switch(selectedAnchor){
