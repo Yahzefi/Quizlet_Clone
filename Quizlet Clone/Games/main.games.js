822c7de4959e75f57a8913711e94530ac7d8e1b9
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-07 21:40:14 -0400

    Update main.games.js

--- Quizlet Clone/Games/main.games.js
+++ Quizlet Clone/Games/main.games.js
@@ -1,7 +1,11 @@
 import { startGameOne } from "./firstGame.js";
 import { startGameTwo } from "./secondGame.js";
 
+// this is the main function for the games page
+// the first game I created as I followed along with a Canvas tutorial on W3Schools
+// Anything afterwards is something I did as an offshoot of that original tutorial/file
+
 $(document).ready(()=>{
-    // startGameOne();
+    startGameOne();
     startGameTwo();
 });
